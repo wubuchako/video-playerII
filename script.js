6244f843fad04a72dcad8a74a192ed179d25e627
github wubuchako <jfdev12@gmail.com>
2020-09-19 20:46:19 +0900

    btn code

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon(){
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
+
+function togglePlay(){
+    if(video.paused){
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    } else {
+        video.pause();     
+    }
+}
+
+// On video End, show play button icon
+video.addEventListener('ended', showPlayIcon);
 
 
 // Progress Bar ---------------------------------- //
 
+//Update progress bar as video plays
+function updateProgress(){
+   progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
 
 
 // Volume Controls --------------------------- //
@@ -18,4 +48,8 @@
 
 // Fullscreen ------------------------------- //
 
-
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
