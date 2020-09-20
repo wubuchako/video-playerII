bca166fca718f0c58607d77a0580d91b8807212c
github wubuchako <jfdev12@gmail.com>
2020-09-20 12:40:54 +0900

    time duration

--- script.js
+++ script.js
@@ -32,9 +32,19 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+//Calculate display time format
+function displayTime(time){
+ const minute = Math.floor(time / 60);
+ let seconds = Math.floor(time % 60);
+ seconds = seconds > 9 ? seconds : `0${seconds}`;
+ return `${minutes} : ${seconds}`;
+}
+
 //Update progress bar as video plays
 function updateProgress(){
    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+   currentTime.textContent = `${displayTime(video.curentTime)}/`;
+   duration.textContent = `${displayTime(video.duration)}`;
 }
 
 
