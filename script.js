9d9a575382a1ba88b1b13177e1c54ff366bc52cb
github wubuchako <jfdev12@gmail.com>
2020-09-20 12:54:14 +0900

    volume icon

--- script.js
+++ script.js
@@ -50,6 +50,29 @@ function updateProgress(){
 
 // Volume Controls --------------------------- //
 
+//Volume Bar
+function changeVolume(e){
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    //Rounding volume up or down
+    if(volume < 0.1){
+        volume =0;
+    }
+    if(volume > 0.9){
+        volume =1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+    console.log(volume);
+    //change icon depending on volume
+    volumeIcon.className = '';
+    if(volume > 0.7){
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+    } else if(volume < 0.7 && volume > 0){
+        volumeIcon.classList.add('fas', 'fa-volume-down');
+    } else if(volume === 0){
+        volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+}
 
 
 // Change Playback Speed -------------------- //
@@ -63,3 +86,5 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
